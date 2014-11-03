4c8c97df16a4675b77af2be663a51303556de104
ndc9jh <ndc9jh@virginia.edu>
2014-11-03 18:19:51 -0500

    Added an Activity for the Store screen
    Buying item methods will go here.

---
+++ GhostHunter/src/edu/virginia/cs2110/ghosthunter/StoreActivity.java
@@ -0,0 +1,15 @@
+package edu.virginia.cs2110.ghosthunter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StoreActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_store);
+	}
+}
