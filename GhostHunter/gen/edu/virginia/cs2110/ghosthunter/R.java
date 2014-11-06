f214a153d429dccd59b8105ee4676f6ce5f7d1cb
xmoonjx <xmoonjx@gmail.com>
2014-11-05 22:20:25 -0500

    D-pad buttons

--- GhostHunter/gen/edu/virginia/cs2110/ghosthunter/R.java
+++ GhostHunter/gen/edu/virginia/cs2110/ghosthunter/R.java
@@ -15,8 +15,13 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int button1=0x7f060006;
         public static final int buttonStart=0x7f060001;
+        public static final int downButton=0x7f060002;
+        public static final int leftButton=0x7f060004;
+        public static final int rightButton=0x7f060003;
         public static final int textView1=0x7f060000;
+        public static final int upButton=0x7f060005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
