f678136f4b108357c41004b5beb23f1c240af1b5
xmoonjx <xmoonjx@gmail.com>
2014-11-06 13:23:04 -0500

    Revised Graphics working

--- GhostHunter/gen/edu/virginia/cs2110/ghosthunter/R.java
+++ GhostHunter/gen/edu/virginia/cs2110/ghosthunter/R.java
@@ -11,18 +11,24 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int gamewallpaper=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int buttona=0x7f020000;
+        public static final int buttonb=0x7f020001;
+        public static final int dpad=0x7f020002;
+        public static final int gamewallpaper=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int buttonA=0x7f060006;
+        public static final int buttonA=0x7f060008;
         public static final int buttonB=0x7f060007;
-        public static final int buttonStart=0x7f060001;
-        public static final int downButton=0x7f060002;
-        public static final int leftButton=0x7f060004;
-        public static final int rightButton=0x7f060003;
-        public static final int textView1=0x7f060000;
-        public static final int upButton=0x7f060005;
+        public static final int buttonStart=0x7f060000;
+        public static final int dPad=0x7f060002;
+        public static final int downButton=0x7f060003;
+        public static final int leftButton=0x7f060005;
+        public static final int padA=0x7f060009;
+        public static final int padB=0x7f06000a;
+        public static final int rightButton=0x7f060004;
+        public static final int textView1=0x7f060001;
+        public static final int upButton=0x7f060006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
