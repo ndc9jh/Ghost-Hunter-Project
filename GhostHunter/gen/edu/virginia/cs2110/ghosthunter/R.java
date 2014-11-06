73bd9a0520c618b436d4556e212bcc3b7f7ea03f
xmoonjx <xmoonjx@gmail.com>
2014-11-06 15:47:09 -0500

    edit dpad name format

--- GhostHunter/gen/edu/virginia/cs2110/ghosthunter/R.java
+++ GhostHunter/gen/edu/virginia/cs2110/ghosthunter/R.java
@@ -13,7 +13,7 @@ public final class R {
     public static final class drawable {
         public static final int buttona=0x7f020000;
         public static final int buttonb=0x7f020001;
-        public static final int dpad=0x7f020002;
+        public static final int directionpad=0x7f020002;
         public static final int gamewallpaper=0x7f020003;
         public static final int ic_launcher=0x7f020004;
     }
