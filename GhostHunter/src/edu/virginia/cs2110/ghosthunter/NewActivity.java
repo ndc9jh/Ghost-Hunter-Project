4a36c62cf7bd9322c829fcae5f1849573d129a4e
xmoonjx <xmoonjx@gmail.com>
2014-11-06 16:10:24 -0500

    Actions skeleton for later implementation
    Currently has toasts

--- GhostHunter/src/edu/virginia/cs2110/ghosthunter/NewActivity.java
+++ GhostHunter/src/edu/virginia/cs2110/ghosthunter/NewActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class NewActivity extends MainActivity {
 
@@ -21,6 +23,41 @@ public class NewActivity extends MainActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new);
 
+	}
+	
+	//Up Button
+	public void up(View v) {
+		Toast.makeText(getApplicationContext(), "UP",
+				   Toast.LENGTH_SHORT).show();
+	}
+	
+	//Down Button
+	public void down(View v) {
+		Toast.makeText(getApplicationContext(), "DOWN",
+				   Toast.LENGTH_SHORT).show();
+	}
+	
+	//Left Button
+	public void left(View v) {
+		Toast.makeText(getApplicationContext(), "LEFT",
+				   Toast.LENGTH_SHORT).show();
+	}
+	//Up Button
+	public void right(View v) {
+		Toast.makeText(getApplicationContext(), "RIGHT",
+				   Toast.LENGTH_SHORT).show();
+	}
+	
+	//A Button
+	public void pressA(View v) {
+		Toast.makeText(getApplicationContext(), "A",
+				   Toast.LENGTH_SHORT).show();
+	}
+	
+	//B Button
+	public void pressB(View v) {
+		Toast.makeText(getApplicationContext(), "B",
+				   Toast.LENGTH_SHORT).show();
 	}
 	
 }
